00d0b107e5a032ade933db39bbf2442307fc0dd7
Duy Tran <txd22081999@gmail.com>
2021-05-03 15:25:44 +0700

    push to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Map from './components/Map'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <div style={{ margin: 20 }}>
+          <Map
+            google={this.props.google}
+            // center={{ lat: 18.5204, lng: 73.8567 }}
+            // 10.7634029,106.6810705
+            center={{ lat: 10.7634029, lng: 106.6810705 }}
+            height='600px'
+            zoom={15}
+          />
+        </div>
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/components/config.js
@@ -0,0 +1,3 @@
+// export const API_KEY = `AIzaSyCwO9C4RJuJIULz7nuKYgRfZKkXvbuUno4`
+export const API_KEY = `AIzaSyDGGYtVIPXFYHJkU5AAVaC0naDfWWtXcfw`
+// export const API_KEY = ``
